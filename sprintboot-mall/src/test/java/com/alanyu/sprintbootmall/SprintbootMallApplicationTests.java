f5cd35b552c4cc6e66327a2416ebea0931722d62
alanyu <aalan50208tw@gmail.com>
2024-03-27 23:16:34 +0800

    t初始化 sprintboot project

---
+++ sprintboot-mall/src/test/java/com/alanyu/sprintbootmall/SprintbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanyu.sprintbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
