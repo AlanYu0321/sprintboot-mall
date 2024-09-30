a56d7598a78b705a51a86122ed2ea25f574e2bba
Tsu-Hao Yu <tyu17@hawk.iit.edu>
2024-09-30 01:10:02 -0500

    front-end integration

--- sprintboot-mall/src/main/java/com/alanyu/sprintbootmall/controller/ProductContoller.java
+++ sprintboot-mall/src/main/java/com/alanyu/sprintbootmall/controller/ProductContoller.java
@@ -2,26 +2,32 @@ package com.alanyu.sprintbootmall.controller;
 
 import com.alanyu.sprintbootmall.model.Product;
 import com.alanyu.sprintbootmall.sevice.ProductService;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductContoller {
 
 	@Autowired
 	private ProductService productService;
 
 	@PostMapping(value = "/products")
-	public ResponseEntity<Product> getProduct(@RequestBody Product product) {
+	public ResponseEntity<Map<String, Product>> getProduct(@RequestBody Product product) {
 
 		Product responseProduct = productService.getProductById(product.getProductId());
 
 		if (responseProduct != null) {
-			return ResponseEntity.status(HttpStatus.OK).body(responseProduct);
+			Map<String, Product> response = new HashMap<>();
+			response.put("reply", responseProduct);
+			return ResponseEntity.ok(response);
 		} else {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
@@ -63,5 +69,17 @@ public class ProductContoller {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 
+	}
+
+	@PostMapping(value = "/helloWorld")
+	public ResponseEntity<Map<String, String>> helloWorld(
+			@RequestBody Map<String, String> requestData) {
+
+		// 從請求中獲取資料
+		String clientMessage = requestData.get("message");
+
+		Map<String, String> response = new HashMap<>();
+		response.put("reply", "Hello, Vue!" + clientMessage);
+		return ResponseEntity.ok(response);
 	}
 }
